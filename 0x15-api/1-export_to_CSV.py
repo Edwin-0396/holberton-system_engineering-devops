71e5bb2a50f0e62d26f1eb8af98810e1238368af
Edwin-9603 <egc4611@gmail.com>
2022-09-08 16:06:14 -0400

    new updates

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress."""
+
+if __name__ == "__main__":
+	import csv
+	import requests
+	import sys
+
+	USER_ID = sys.argv[1]
+
+	todos = requests.get(f'https://jsonplaceholder.typicode.com/todos')
+	users = requests.get(f'https://jsonplaceholder.typicode.com/users/{USER_ID}')
+	todos_Json = todos.json()
+	users_Json = users.json()
+
+	USERNAME = users_Json['name']
+
+	with open('USER_ID.csv', 'w', newline='') as csvfile:
+		for items in todos_Json:
+			Uid = items['userId']
+			if (Uid == int(USER_ID)):
+					TASK_COMPLETED_STATUS = items['completed']
+					TASK_TITLE = items['title']
+					spamwriter = csv.writer(csvfile, delimiter=',',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+					spamwriter.writerow([f'"{USER_ID}"', f'"{USERNAME}"', f'"{TASK_COMPLETED_STATUS}"', f'"{TASK_TITLE}"'])
+
+
+
+
+	
